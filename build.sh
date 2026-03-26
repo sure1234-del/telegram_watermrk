b878ce4b6dbed4e8e40511120978245375fa4aed
sure1234-del <suan07244@gmail.com>
2026-03-27 00:04:16 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 apt-get update
-apt-get install -y ffmpeg
+apt-get install -y ffmpeg python3.10 python3.10-venv python3.10-dev
+
+# Set python 3.10 as default
+update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 1
+
+python3 --version
+
 pip install -r requirements.txt
