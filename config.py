3d81643463b1685a810a18934fa224d5c44d8700
sure1234-del <suan07244@gmail.com>
2026-03-26 21:40:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("8715679380:AAEqVan2Zd4Carx2ILLs01AxTEb56rfqIKI")
+API_ID = int(os.getenv("36014101"))
+API_HASH = os.getenv("d4b9c4246a4a0d16e960677725e8ed8e")
 
-OWNER_ID = int(os.getenv("OWNER_ID"))
+OWNER_ID = int(os.getenv("1308571847"))
 
 FORCED_WATERMARK = "Professor"
 
