35533ecf051f9edd5bd497e4989377bbf03ee50e
sure1234-del <suan07244@gmail.com>
2026-03-26 21:31:07 +0530

    Update config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+
+OWNER_ID = int(os.getenv("OWNER_ID"))
+
+FORCED_WATERMARK = "Professor"
+
+CHANNEL_ID = os.getenv("CHANNEL_ID")
